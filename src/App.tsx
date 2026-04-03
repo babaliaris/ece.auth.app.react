8f754989f60e7147d6e0d5f2b5b32bd1d5f6c43e
babaliaris <babaliaris.nikos@gmail.com>
2026-04-03 17:48:37 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+function App() {
+
+  return (
+    <>
+      <div
+      style={
+        {
+          width: "50%",
+          marginLeft: "auto",
+          marginRight: "auto"
+        }
+      }
+      >
+        Hello ece.auth.app.react
+      </div>
+    </>
+  )
+}
+
+export default App;
