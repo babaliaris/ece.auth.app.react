64d1f03628c540685e26ebb1a626b8ce81fd1098
babaliaris <babaliaris.nikos@gmail.com>
2026-04-06 12:56:01 +0300

    Login/Register auth state integration + redirections

--- src/Root/Login/LoginRoute.tsx
+++ src/Root/Login/LoginRoute.tsx
@@ -1,22 +1,52 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
+import { useNavigate } from "react-router";
 import LoginUI, { type LoginDataType } from "./LoginUI.tsx";
+import { ECE_ROUTE_PATHS } from "@/core/routes.tsx";
 import { ece_api } from "@/core/services/api/api.service.ts";
 import { eceApiGetErrorInfo } from "@/core/services/api/api-error-handler.service.ts";
 import { ece_logger } from "@/core/logger.core.ts";
+import { useEceAuthStore } from "@/core/zustand-state.ts";
 
 function LoginRoute()
 {
+  const navigate = useNavigate();
+
+  const {
+    setAuth,
+    setInitialized,
+    is_authenticated
+  } = useEceAuthStore();
+
+  useEffect(() =>
+  {
+    if (is_authenticated)
+    {
+      // TODO: Change this to take them to the STUDENT or ADMIN home page.
+      navigate(ECE_ROUTE_PATHS.ROOT, { replace: true });
+    }
+  }, [is_authenticated, navigate]);
+
   const onLogin = useCallback(async (value: LoginDataType)=>
   {
+    // TODO: Use a loading spinner.
     const result = await ece_api.userLogin(
     {
-      m_email: value.email,
-      m_pass: value.password
+      m_email : value.email,
+      m_pass  : value.password
     });
 
     if (result.success && result.data)
     {
       ece_logger.info('UserLogin: ', result.data);
+
+      setAuth(
+      {
+        m_uuid  : result.data.body.m_uuid,
+        m_email : result.data.body.m_email,
+        m_role  : result.data.body.m_role
+      });
+
+      setInitialized(true);
     }
 
     else
--- src/Root/Register/RegisterRoute.tsx
+++ src/Root/Register/RegisterRoute.tsx
@@ -1,13 +1,18 @@
 import { useCallback } from "react";
+import { useNavigate } from "react-router";
 import RegisterUI, { type RegisterDataType } from "./RegisterUI.tsx";
 import { ece_api } from "@/core/services/api/api.service.ts";
 import { eceApiGetErrorInfo } from "@/core/services/api/api-error-handler.service.ts";
 import { ece_logger } from "@/core/logger.core.ts";
+import { ECE_ROUTE_PATHS } from "@/core/routes.tsx";
 
 function RegisterRoute()
 {
+  const navigate = useNavigate();
+
   const onRegister = useCallback(async (value: RegisterDataType)=>
   {
+    //TODO: Use a loading spinner.
     const result = await ece_api.userPost(
     {
       m_email: value.email,
@@ -17,6 +22,7 @@ function RegisterRoute()
     if (result.success && result.data)
     {
       ece_logger.info("UserPosted: ", result.data);
+      navigate(ECE_ROUTE_PATHS.LOGIN);
     }
 
     else
